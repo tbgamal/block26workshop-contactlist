96b03017efcb83a642a4e281fe5f1fd648dbb642
tbgamal <tbgamalm@gmail.com>
2023-11-07 13:44:55 -0600

    committing progress after successfully run ContactRow.jsx

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import ContactRow from "./ContactRow"
+
+const dummyContacts =[
+  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+]
+
+function ContactList () {
+  
+  const [contacts, setContacts] = useState(dummyContacts)
+  console.log("Contacts: ", contacts)
+  return (
+  <>
+    <table>
+      <thead>
+        <tr>
+          <th colSpan="3">Contact List</th>
+        </tr>
+        <tr>
+          <td>name</td>
+          <td>email</td>
+          <td>phone</td>
+        </tr>
+      </thead>
+      <tbody>
+        {contacts.map((contact) => {
+          return <ContactRow key={contact.id} contact={contact} />
+        })}
+      </tbody>
+    </table>
+  </>
+  )
+}
+
+export default ContactList
---
+++ src/components/ContactRow.jsx
@@ -0,0 +1,14 @@
+
+
+function ContactRow ({ contact }) {
+  return (
+    <tr>
+      <td>{contact.name}</td>
+      <td>{contact.email}</td>
+      <td>{contact.phone}</td>
+    </tr>
+  )
+
+}
+
+export default ContactRow
